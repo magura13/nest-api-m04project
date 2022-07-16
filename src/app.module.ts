ef8c8f41540c8f7f186a714134559546a6c1f2cc
magura13 <tiagolelis_@hotmail.com>
2022-07-16 17:53:29 -0300

    feat implement new dtos

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,12 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { EventModule } from './event/event.module';
+import { ProfileModule } from './profile/profile.module';
+import { GenresModule } from './genres/genres.module';
+import { FavoritesModule } from './favorites/favorites.module';
 
 @Module({
-  imports: [UserModule, PrismaModule, EventModule],
+  imports: [UserModule, PrismaModule, EventModule, ProfileModule, GenresModule, FavoritesModule],
   controllers: [AppController],
   providers: [AppService],
 })
